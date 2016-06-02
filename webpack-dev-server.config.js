fa21e71e6118b7a7dac6be88f526c6c69f408e25
gaotianyi <gaotianyi@bytedance.com>
2016-06-02 19:55:53 +0800

    webpack.config
    Change-Id: I7dfdf4a4ddaba5127f683f49cdffc8ac59b14ad1

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -2,26 +2,18 @@ var webpack = require('webpack');
 
 // nodejspath 模块
 var path = require('path');
-var buildPath = path.resolve(__dirname, 'build');
 
 // webpack 若需要迁移文件操作，需要加载transfer-webpack-plugin 插件
 var TransferWebpackPlugin = require('transfer-webpack-plugin');
 
 module.exports = {
-    // 入口文件
-    entry: [
-        'webpack/hot/dev-server',
-        'webpack/hot/only-dev-server',
-        './src/projects/batch_ban/app.jsx'
-    ],
-    // 配置加载后缀名
+    entry: './src/app.jsx',
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
     resolve: {
         extensions: ["", ".js", ".jsx"]
-    },
-    // 产出路径
-    output: {
-        path: buildPath,
-        filename: "app.js"
     },
     module: {
         loaders: [{
@@ -36,14 +28,5 @@ module.exports = {
             }
         }]
     },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new TransferWebpackPlugin([{
-            from: 'www'
-        }], path.resolve(__dirname, "src/projects/batch_ban")),
-        new TransferWebpackPlugin([{
-            from: './',
-            to: 'static'
-        }], path.resolve(__dirname, "src/static"))
-    ]
+    plugins: []
 };
