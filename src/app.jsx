675bbc09268f1f03a7107b37217ba12c4df69f5d
gaotianyi <gaotianyi@bytedance.com>
2016-06-07 18:19:57 +0800

    init
    Change-Id: Id090f02f83c42fde13133f6d6c61c7d35381455b

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,24 @@
-import  React from 'react';
-class Photo extends React.Component {
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TinyMCE from 'react-tinymce';
 
-}
-exports.Photo = Photo;
+const App = React.createClass({
+    handleEditorChange(e) {
+        console.log('Content was updated:', e.target.getContent());
+    },
+
+    render() {
+        return (
+            <TinyMCE
+                content="<p>This is the initial content of the editor</p>"
+                config={{
+          plugins: 'link image code',
+          toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+        }}
+                onChange={this.handleEditorChange}
+            />
+        );
+    }
+});
+
+ReactDOM.render(<App/>, document.getElementById('container'));
--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -1,32 +1,19 @@
+var path = require('path');
 var webpack = require('webpack');
 
-// nodejspath 模块
-var path = require('path');
-
-// webpack 若需要迁移文件操作，需要加载transfer-webpack-plugin 插件
-var TransferWebpackPlugin = require('transfer-webpack-plugin');
-
 module.exports = {
-    entry: path.resolve(__dirname, 'src/app.jsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js"
-    },
+    entry: './src/app.jsx',
+    output: { path: __dirname, filename: 'bundle.js' },
     module: {
         loaders: [
             {
-                test: /\.(css|less)$/,
-                loader: 'style-loader!css-loader!less-loader'
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: ['babel-loader'],
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
                 query: {
-                    presets: ['react', 'es2015']
+                    presets: ['es2015', 'react']
                 }
             }
         ]
     },
-    plugins: []
 };
