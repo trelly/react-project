e5fa80bfbff1f7eb0419a56365929dff2beaa5e7
gaotianyi <gaotianyi@bytedance.com>
2016-06-13 19:33:54 +0800

    init
    Change-Id: I29e30bf2bf9f89dd2aa0d67ea2aee3d93030caad

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react'
+
+class Header extends Component {
+    render() {
+        const value = this.props.value;
+        return (
+            <div>
+                this is a cover set!
+            </div>
+        )
+    }
+}
+
+export default Cover
---
+++ src/containers/Editor.jsx
@@ -0,0 +1,9 @@
+import { Component } from 'react'
+
+class Editor extends Component {
+    render() {
+        return 
+    }
+
+
+}
