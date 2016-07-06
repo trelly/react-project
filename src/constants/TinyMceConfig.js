efc88e29bc9a3be2dd601af65730f3d40d4a961a
gaotianyi <gaotianyi@bytedance.com>
2016-07-06 20:15:23 +0800

    tinymce css
    Change-Id: I32286519b69aed211c7a4d6590291a52ebc67be6

--- src/constants/TinyMceConfig.js
+++ src/constants/TinyMceConfig.js
@@ -1,7 +1,7 @@
 const CONFIG = {
     plugins: "textpattern, image, link, media, preview, anchor, autosave, autoresize",
     menubar: false,
-    toolbar: ['undo redo | bold underline italic | blockquote bullist numlist alignleft aligncenter alignright', 'link image media | preview'],
+    toolbar: ['newdocument | undo redo | bold underline italic | blockquote bullist numlist alignleft aligncenter alignright', 'link image media | preview'],
     autosave_restore_when_empty: false,
     autosave_ask_before_unload: true,
     autosave_interval: "5s",
